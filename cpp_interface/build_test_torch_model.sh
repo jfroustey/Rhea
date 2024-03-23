1979f34626c7e39e672eae8f8ea36ec57d06a256
Julien Froustey <jfroust@spica.physics.ncsu.edu>
2024-03-23 14:16:30 -0400

    Build and CMakelist for spica

--- cpp_interface/build_test_torch_model.sh
+++ cpp_interface/build_test_torch_model.sh
@@ -1,3 +1,3 @@
-/home/srichers/.local/bin/cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/gcc -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/g++ -S/mnt/scratch/srichers/software/Rhea/cpp_interface -B/mnt/scratch/srichers/software/Rhea/build -G Ninja
+~/miniconda3/envs/mlenv/bin/cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/gcc -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/g++ -S~/Documents/Rhea/cpp_interface -B~/Documents/Rhea/build -G Ninja
 
-/home/srichers/.local/bin/cmake --build /mnt/scratch/srichers/software/Rhea/build --config Debug --target all --
+~/miniconda3/envs/mlenv/bin/cmake --build ~/Documents/Rhea/build --config Debug --target all --
